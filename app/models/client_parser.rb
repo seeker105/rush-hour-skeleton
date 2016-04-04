e01770ea6f8ad580d365d4d5d79175cc0f8f9ab4
Christopher Soden <karavol27@gmail.com>
2016-04-04 11:25:52 -0600

    refactored parse_client_and_direct_to_page method

--- app/models/client_parser.rb
+++ app/models/client_parser.rb
@@ -1,24 +1,12 @@
 module ClientParser
 
-	#BREAK UP INTO SMALLER METHODS
-	def parse_client_and_direct_to_page
-		@client = Client.find_by(identifier: params['identifier'])
-		@identifier = @client.identifier if @client
-
-		if @client == nil
-			erb :not_registered
-		elsif @client.payload_requests.count == 0
-			erb :no_data
-		elsif @client
-			urls = @client.most_to_least_frequent_urls
-			@urls_with_requests = Hash.new([])
-			urls.map do |url|
-				@urls_with_requests[url] += @client.find_payload_requests_by_relative_path(url) if @client.find_payload_requests_by_relative_path(url)
-			end
-
-			@relativepaths = @urls_with_requests.keys.map { |url| URI.parse(url).path }
-			erb :dashboard
+	def get_relative_paths(client)
+		urls = client.most_to_least_frequent_urls
+		@urls_with_requests = Hash.new([])
+		urls.map do |url|
+			@urls_with_requests[url] += client.find_payload_requests_by_relative_path(url) if @client.find_payload_requests_by_relative_path(url)
 		end
+		@urls_with_requests.keys.map { |url| URI.parse(url).path }
 	end
 
 	def parse_event_data(identifier, eventname)
@@ -30,7 +18,7 @@ module ClientParser
 
 	def client_events_exist?(client, eventname)
 		if client && client.events.find_by(name: eventname)
-			event_hours = find_and_format_event_hours(eventname)
+			event_hours = find_and_format_event_hours(client, eventname)
 			create_events_by_hour_hash(event_hours)
 			true
 		else
